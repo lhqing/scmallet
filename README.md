146924d565868e1831ba5b1e2e1860db108c544d
hq <liuhq@ucsd.edu>
2024-03-26 23:22:58 -0400

    init

---
+++ README.md
@@ -0,0 +1,56 @@
+# scmallet
+
+[![Tests][badge-tests]][link-tests]
+[![Documentation][badge-docs]][link-docs]
+
+[badge-tests]: https://img.shields.io/github/actions/workflow/status/lhqing/scmallet/test.yaml?branch=main
+[link-tests]: https://github.com/lhqing/scmallet/actions/workflows/test.yml
+[badge-docs]: https://img.shields.io/readthedocs/scmallet
+
+Python wrapper of MALLET for LDA analysis on single-cell data
+
+## Getting started
+
+Please refer to the [documentation][link-docs]. In particular, the
+
+-   [API documentation][link-api].
+
+## Installation
+
+You need to have Python 3.9 or newer installed on your system. If you don't have
+Python installed, we recommend installing [Mambaforge](https://github.com/conda-forge/miniforge#mambaforge).
+
+There are several alternative options to install scmallet:
+
+<!--
+1) Install the latest release of `scmallet` from `PyPI <https://pypi.org/project/scmallet/>`_:
+
+```bash
+pip install scmallet
+```
+-->
+
+1. Install the latest development version:
+
+```bash
+pip install git+https://github.com/lhqing/scmallet.git@main
+```
+
+## Release notes
+
+See the [changelog][changelog].
+
+## Contact
+
+For questions and help requests, you can reach out in the [scverse discourse][scverse-discourse].
+If you found a bug, please use the [issue tracker][issue-tracker].
+
+## Citation
+
+> t.b.a
+
+[scverse-discourse]: https://discourse.scverse.org/
+[issue-tracker]: https://github.com/lhqing/scmallet/issues
+[changelog]: https://scmallet.readthedocs.io/latest/changelog.html
+[link-docs]: https://scmallet.readthedocs.io
+[link-api]: https://scmallet.readthedocs.io/latest/api.html
---
+++ src/scmallet/utils.py
@@ -0,0 +1,46 @@
+import pathlib
+import subprocess
+
+
+def get_mallet_path():
+    """Get the path to the Mallet executable."""
+    try:
+        path = subprocess.check_output(["which", "mallet"], encoding="utf8")
+    except subprocess.CalledProcessError:
+        print(
+            "LDA package Mallet not found. Please install it with `conda install -c conda-forge mallet` or `mamba install -c conda-forge mallet`"
+        )
+    return path.strip()
+
+
+MALLET_PATH = get_mallet_path()
+MALLET_PATH = pathlib.Path(MALLET_PATH)
+_mallet_grandparent = MALLET_PATH.parent.parent
+MALLET_JAVA_BASE = (
+    "java "
+    "-Xmx{mem_gb}g "
+    "-ea "
+    "-Djava.awt.headless=true "
+    "-Dfile.encoding=UTF-8 "
+    "-server "
+    f"-classpath {_mallet_grandparent}/class:{_mallet_grandparent}/lib/mallet-deps.jar: "
+    "{mallet_cmd} "
+)
+
+
+MALLET_COMMAND_MAP = {
+    "import-dir": "cc.mallet.classify.tui.Text2Vectors",
+    "import-file": "cc.mallet.classify.tui.Csv2Vectors",
+    "import-svmlight": "cc.mallet.classify.tui.SvmLight2Vectors",
+    "info": "cc.mallet.classify.tui.Vectors2Info",
+    "train-classifier": "cc.mallet.classify.tui.Vectors2Classify",
+    "classify-dir": "cc.mallet.classify.tui.Text2Classify",
+    "classify-file": "cc.mallet.classify.tui.Csv2Classify",
+    "classify-svmlight": "cc.mallet.classify.tui.SvmLight2Classify",
+    "train-topics": "cc.mallet.topics.tui.TopicTrainer",
+    "infer-topics": "cc.mallet.topics.tui.InferTopics",
+    "evaluate-topics": "cc.mallet.topics.tui.EvaluateTopics",
+    "prune": "cc.mallet.classify.tui.Vectors2Vectors",
+    "split": "cc.mallet.classify.tui.Vectors2Vectors",
+    "bulk-load": "cc.mallet.util.BulkLoader",
+}
